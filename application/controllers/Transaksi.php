b6a7e3f9320efa954349a5731e4829cbc6287d27
ifaliefya <aliefya.fadhila@gmail.com>
2018-04-19 20:30:17 +0700

    Transaksi
    Commit pertama bismillah

---
+++ application/controllers/Transaksi.php
@@ -0,0 +1,17 @@
+<?php
+
+class Transaksi extends CI_Controller {
+
+	public function index()
+		{
+			$this->load->view('header');
+			$this->load->view('transaksi_petani');
+			$this->load->view('footer');
+		}
+
+	public function transborong()
+		{
+			$this->load->view('transaksi_pemborong');
+		}
+
+}
---
+++ application/views/transaksi_petani.php
@@ -0,0 +1,412 @@
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Widgets
+        <small>Preview page</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Widgets</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+      <div class="row">
+        <div class="col-md-9 col-sm-9 col-xs-12">
+          <div class="judul-box">
+            <span class="judul-box-icon bg-teal">
+              <span class="judul-box-number" style="margin-top: 10%">23</span>
+              <span class="judul-box-text">Oktober</span>
+            </span> 
+            <div class="judul-box-content">
+              <div class="row">
+                <div class="col-md-7 col-sm-5 col-xs-12 col-md-offset-2">
+                  
+                  <div class="inpo-box bg-green"> 
+                    <span class="inpo-box-icon"> <i class="fa fa-male"></i> </span>
+                    <div class="inpo-box-content">
+                      <span class="inpo-box-text">Jumlah penyetor hari ini</span>
+                      <span class="inpo-box-number">23</span>
+                    </div>
+                  </div>
+                </div>
+
+                  <div class="row">
+                    <div class="col-md-5 col-sm-12 ">
+
+                      <div class="inpo-box"> 
+                        <span class="inpo-box-icon bg-blue"> <i class="fa fa-download"></i> </span>
+                        <div class="inpo-box-content">
+                          <span class="inpo-box-text">Cabai setoran</span>
+                          <span class="inpo-box-number">155 kg</span>
+                        </div>
+                      </div>
+
+                      <div class="inpo-box"> 
+                        <span class="inpo-box-icon bg-aqua"> <i class="fa fa-upload"></i> </span>
+                        <div class="inpo-box-content">
+                          <span class="inpo-box-text">Cabai terjual</span>
+                          <span class="inpo-box-number">100 kg</span>
+                        </div>
+                      </div>
+                    </div>
+
+
+                    <div class="col-md-5 col-sm-12">
+                      <div class="inpo-box"> 
+                        <span class="inpo-box-icon bg-blue"> <i class="glyphicon glyphicon-upload"></i> </span>
+                        <div class="inpo-box-content">
+                          <span class="inpo-box-text">Uang keluar</span>
+                          <span class="inpo-box-number">Rp. 6,890,000</span>
+                        </div>
+                      </div>
+
+                      <div class="inpo-box"> 
+                        <span class="inpo-box-icon bg-aqua"> <i class="glyphicon glyphicon-download"></i> </span>
+                        <div class="inpo-box-content">
+                          <span class="inpo-box-text">Uang yang diterima</span>
+                          <span class="inpo-box-number">Rp. 16,900,000</span>
+                        </div>
+                      </div>
+
+                    </div>
+                  </div>
+              </div>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+
+        <!-- /.col -->
+        <div class="col-md-3 col-sm-3 col-xs-6">
+          <div class="box box-info">
+            <div class="box-header">
+              <div class="box-title"> Masukkan setoran 
+              </div>
+            </div>
+
+            <div class="box-body" style="height: 160px; line-height:130px; text-align: center;">
+              <div class="btn-group">
+                <button type="button" class="btn btn-setor bg-green"> <i class="fa fa-plus"></i> Inputkan Setoran</button>
+                <button type="button" class="btn btn-setor bg-yellow"> <i class="fa fa-user-plus"></i> Tambah petani</button>
+              </div>
+<!--               <a class="btn btn-setor">
+                <i class="fa fa-edit"></i> Edit
+              </a> -->
+            </div>
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="clearfix"> </div>
+      </div>
+      <!-- /.row -->
+
+<!-- tabel -->
+ <div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title">Catatan harian</h3>
+
+              <div class="box-tools">
+                <div class="input-group input-group-sm" style="width: 150px;">
+                  <input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
+
+                  <div class="input-group-btn">
+                    <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body table-responsive no-padding">
+              <table class="table table-hover" style="font-size: 15px">
+                <tr class="bg-gray">
+                  <th style="text-align: center; line-height: 50px" rowspan="2">ID</th>
+                  <th style="text-align: center; line-height: 50px" rowspan="2">Nama</th>
+                  <th style="text-align: center; line-height: 50px" rowspan="2">Asal Daerah</th>
+                  <th style="text-align: center;" colspan="3">Berat</th>
+                  <th style="text-align: center; line-height: 50px" rowspan="2">Harga</th>
+                  <th style="text-align: center; line-height: 50px" rowspan="2">Jumlah Uang</th>
+                  <th style="text-align: center; line-height: 50px" rowspan="2">Bon</th>
+                  <th style="text-align: center; line-height: 50px" rowspan="2">Saldo</th>
+                </tr>
+                <tr class="bg-gray">
+                  <th>Kotor </th>
+                  <th>BS/ MTL </th>
+                  <th>Bersih </th>
+                </tr>
+                <tr>
+                  <td>183</td>
+                  <td>Menik</td>
+                  <td>Gombong</td>
+                  <td>28,5</td>
+                  <td>2</td>
+                  <td>26</td>
+                  <td>54000</td>
+                  <td>1.404.000</td>
+                  <td>35.000.000</td>
+                  <td class="text-success"><strong>D 22.9222.000</strong></td>
+                </tr>
+                <tr>
+                  <td>219</td>
+                  <td>Annisa Amalia</td>
+                  <td>Talun</td>
+                  <td>28,5</td>
+                  <td>2</td>
+                  <td>26</td>
+                  <td>54000</td>
+                  <td>1.404.000</td>
+                  <td>35.000.000</td>
+                  <td class="text-success"><strong>D 22.9222.000</strong></td>
+                </tr>
+                <tr>
+                  <td>657</td>
+                  <td>Thufalifa</td>
+                  <td>Muntilan</td>
+                  <td>28,5</td>
+                  <td>2</td>
+                  <td>26</td>
+                  <td>54000</td>
+                  <td>1.404.000</td>
+                  <td>35.000.000</td>
+                  <td class="text-success"><strong>D 22.9222.000</strong></td>
+                </tr>
+                <tr>
+                  <td>175</td>
+                  <td>Paijo</td>
+                  <td>Bantul</td>
+                  <td>28,5</td>
+                  <td>2</td>
+                  <td>26</td>
+                  <td>54000</td>
+                  <td>1.404.000</td>
+                  <td>35.000.000</td>
+                  <td class="text-success"><strong>D 22.9222.000</strong></td>
+                </tr>
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+      </div>
+
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 2.4.0
+    </div>
+    <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+    reserved.
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Create the tabs -->
+    <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
+      <li><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
+      <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
+    </ul>
+    <!-- Tab panes -->
+    <div class="tab-content">
+      <!-- Home tab content -->
+      <div class="tab-pane" id="control-sidebar-home-tab">
+        <h3 class="control-sidebar-heading">Recent Activity</h3>
+        <ul class="control-sidebar-menu">
+          <li>
+            <a href="javascript:void(0)">
+              <i class="menu-icon fa fa-birthday-cake bg-red"></i>
+
+              <div class="menu-info">
+                <h4 class="control-sidebar-subheading">Langdon's Birthday</h4>
+
+                <p>Will be 23 on April 24th</p>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0)">
+              <i class="menu-icon fa fa-user bg-yellow"></i>
+
+              <div class="menu-info">
+                <h4 class="control-sidebar-subheading">Frodo Updated His Profile</h4>
+
+                <p>New phone +1(800)555-1234</p>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0)">
+              <i class="menu-icon fa fa-envelope-o bg-light-blue"></i>
+
+              <div class="menu-info">
+                <h4 class="control-sidebar-subheading">Nora Joined Mailing List</h4>
+
+                <p>nora@example.com</p>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0)">
+              <i class="menu-icon fa fa-file-code-o bg-green"></i>
+
+              <div class="menu-info">
+                <h4 class="control-sidebar-subheading">Cron Job 254 Executed</h4>
+
+                <p>Execution time 5 seconds</p>
+              </div>
+            </a>
+          </li>
+        </ul>
+        <!-- /.control-sidebar-menu -->
+
+        <h3 class="control-sidebar-heading">Tasks Progress</h3>
+        <ul class="control-sidebar-menu">
+          <li>
+            <a href="javascript:void(0)">
+              <h4 class="control-sidebar-subheading">
+                Custom Template Design
+                <span class="label label-danger pull-right">70%</span>
+              </h4>
+
+              <div class="progress progress-xxs">
+                <div class="progress-bar progress-bar-danger" style="width: 70%"></div>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0)">
+              <h4 class="control-sidebar-subheading">
+                Update Resume
+                <span class="label label-success pull-right">95%</span>
+              </h4>
+
+              <div class="progress progress-xxs">
+                <div class="progress-bar progress-bar-success" style="width: 95%"></div>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0)">
+              <h4 class="control-sidebar-subheading">
+                Laravel Integration
+                <span class="label label-warning pull-right">50%</span>
+              </h4>
+
+              <div class="progress progress-xxs">
+                <div class="progress-bar progress-bar-warning" style="width: 50%"></div>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="javascript:void(0)">
+              <h4 class="control-sidebar-subheading">
+                Back End Framework
+                <span class="label label-primary pull-right">68%</span>
+              </h4>
+
+              <div class="progress progress-xxs">
+                <div class="progress-bar progress-bar-primary" style="width: 68%"></div>
+              </div>
+            </a>
+          </li>
+        </ul>
+        <!-- /.control-sidebar-menu -->
+
+      </div>
+      <!-- /.tab-pane -->
+      <!-- Stats tab content -->
+      <div class="tab-pane" id="control-sidebar-stats-tab">Stats Tab Content</div>
+      <!-- /.tab-pane -->
+      <!-- Settings tab content -->
+      <div class="tab-pane" id="control-sidebar-settings-tab">
+        <form method="post">
+          <h3 class="control-sidebar-heading">General Settings</h3>
+
+          <div class="form-group">
+            <label class="control-sidebar-subheading">
+              Report panel usage
+              <input type="checkbox" class="pull-right" checked>
+            </label>
+
+            <p>
+              Some information about this general settings option
+            </p>
+          </div>
+          <!-- /.form-group -->
+
+          <div class="form-group">
+            <label class="control-sidebar-subheading">
+              Allow mail redirect
+              <input type="checkbox" class="pull-right" checked>
+            </label>
+
+            <p>
+              Other sets of options are available
+            </p>
+          </div>
+          <!-- /.form-group -->
+
+          <div class="form-group">
+            <label class="control-sidebar-subheading">
+              Expose author name in posts
+              <input type="checkbox" class="pull-right" checked>
+            </label>
+
+            <p>
+              Allow the user to show his name in blog posts
+            </p>
+          </div>
+          <!-- /.form-group -->
+
+          <h3 class="control-sidebar-heading">Chat Settings</h3>
+
+          <div class="form-group">
+            <label class="control-sidebar-subheading">
+              Show me as online
+              <input type="checkbox" class="pull-right" checked>
+            </label>
+          </div>
+          <!-- /.form-group -->
+
+          <div class="form-group">
+            <label class="control-sidebar-subheading">
+              Turn off notifications
+              <input type="checkbox" class="pull-right">
+            </label>
+          </div>
+          <!-- /.form-group -->
+
+          <div class="form-group">
+            <label class="control-sidebar-subheading">
+              Delete chat history
+              <a href="javascript:void(0)" class="text-red pull-right"><i class="fa fa-trash-o"></i></a>
+            </label>
+          </div>
+          <!-- /.form-group -->
+        </form>
+      </div>
+      <!-- /.tab-pane -->
+    </div>
+  </aside>
+  <!-- /.control-sidebar -->
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
